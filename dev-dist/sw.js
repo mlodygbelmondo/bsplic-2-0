ea4901fb9331d5d3cd60ee0a95abb5a02d91d279
Piotr Kaczorowski <piotrkaczoroww.ski@gmail.com>
2026-03-24 14:30:16 +0100

    improvement: gitignore update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ad8niq392o8"
+    "revision": "0.gtprvlbn5go"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
