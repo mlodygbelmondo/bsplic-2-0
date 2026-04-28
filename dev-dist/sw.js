0fbb837c66fd8b58f33b8b99c10a3fd896f21bd0
Piotr Kaczorowski <piotrkaczoroww.ski@gmail.com>
2026-04-28 23:22:59 +0200

    feat: adjusted roulette positioning

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.kfcd5ekkgt"
+    "revision": "0.706g74qls1"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
